03a60e787d4bb8349f51a2f8d62da4f998869803
ArvinZaker <arv.zaker@gmail.com>
2024-07-03 19:00:17 -0400

    added conf and README

---
+++ README.md
@@ -0,0 +1,94 @@
+# Step 1: Install dependencies
+
+## Install git (optional)
+
+Please google this to install git for your system.
+
+## Install nix
+
+This info is from the [nixos website](https://nixos.org/download)
+
+### Windows
+If you have windows, first install [Windows Subsystem for linux](https://learn.microsoft.com/en-us/windows/wsl/install)
+
+Then press start and open the linux terminal.
+
+In the terminal, run the following command:
+
+```
+sh <(curl -L https://nixos.org/nix/install) --daemon
+```
+
+### MacOS
+
+Open terminal and run:
+
+```
+sh <(curl -L https://nixos.org/nix/install)
+```
+
+### Linux
+
+Install the nix from your package manager. If not available, run:
+
+```
+sh <(curl -L https://nixos.org/nix/install) --daemon
+```
+
+## Enable nix flakes
+
+In the same terminal, run the following two commands to enable flakes:
+
+```
+mkdir -p ~/.config/nix
+echo "experimental-features = nix-command flakes" >> ~/.config/nix/nix.conf
+```
+# Step 2: Setup the project folder
+
+## Download the data files
+
+Download the files  
+Make sure all the `.fastq.gz` files are in the same place as each other.
+
+## Download the project scripts
+
+
+## Place them in one place
+
+
+## Open the terminal in the project folder
+
+
+
+# Step 2: Setup the development environment
+
+In your project folder, simplify run
+
+```
+nix develop
+```
+
+After you enter the nix shell, run:
+
+```
+curl -s https://get.nextflow.io | bash
+```
+
+And thats it! The beauty of nix allows it to be this simple to make everything ready.
+
+# Run the analysis
+
+We have a 
+
+```
+./nextflow run nf-core/cutandrun -r 3.2.1 --input ./sheet.csv  --outdir ./results/ --genome GRCh38 -profile docker -c ./nextflow.config
+```
+
+# MISC
+
+If you want a one-command to put for compute canada, use this command:
+
+
+```
+nix develop --command curl -s https://get.nextflow.io | bash && chmod +x ./nextflow && ./nextflow run nf-core/cutandrun -r 3.2.1 --input ./sheet.csv  --outdir ./results/ --genome GRCh38 -profile docker
+```
---
+++ flake.nix
@@ -0,0 +1,100 @@
+{
+  description = "Cut&Run development environment";
+
+  inputs = {
+    # Specify the Nixpkgs input
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config = {allowUnfree = true;};
+    };
+  in {
+    devShells.x86_64-linux = {
+      default = pkgs.mkShell {
+        name = "impurePythonEnv";
+        venvDir = "./.venv";
+
+        # Add your package dependencies here
+        buildInputs = with pkgs; [
+          bedops
+          bedtools
+          bowtie2
+          deeptools
+          jdk22
+          macs2
+          meme-suite
+          perl
+          picard-tools
+          samtools
+          trimmomatic
+
+          R
+          # A Python interpreter including the 'venv' module is required to bootstrap
+          # the environment.
+          python3Packages.python
+
+          # This execute some shell code to initialize a venv in $venvDir before
+          # dropping into the shell
+          python3Packages.venvShellHook
+
+          # Those are dependencies that we would like to use from nixpkgs, which will
+          # add them to PYTHONPATH and thus make them accessible from within the venv.
+          python3Packages.numpy
+          python3Packages.pandas
+          python3Packages.scipy
+          python3Packages.scikit-learn
+          python3Packages.requests
+          python3Packages.torch
+          python3Packages.torchvision
+          python3Packages.hyperopt
+          python3Packages.matplotlib
+          python3Packages.wxPython_4_2
+          python3Packages.setuptools
+
+          # In this particular example, in order to compile any binary extensions they may
+          # require, the Python modules listed in the hypothetical requirements.txt need
+          # the following packages to be installed locally:
+          taglib
+          liblinear
+          openssl
+          git
+          libxml2
+          libxslt
+          libzip
+          zlib
+          libstdcxx5
+          stdenv.cc.cc.lib
+          bash
+          wget
+          zlib
+
+          cargo
+        ];
+
+        LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+
+        # Run this command, only after creating the virtual environment
+        postVenvCreation = ''
+          unset SOURCE_DATE_EPOCH
+          LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib/
+        '';
+
+        # Now we can execute any commands within the virtual environment.
+        # This is optional and can be left out to run pip manually.
+        postShellHook = ''
+          # allow pip to install wheels
+          unset SOURCE_DATE_EPOCH
+          # fixes libstdc++ issues and libgl.so issues
+          LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib/
+        '';
+      };
+    };
+  };
+}
